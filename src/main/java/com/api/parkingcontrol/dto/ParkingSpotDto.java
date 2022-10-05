f6ddd0ce940ed55f1ae9d3622ff0d17970127b3e
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-04 22:56:13 -0300

    Inserindo mudanças feitasa no escopo da dto

--- src/main/java/com/api/parkingcontrol/dto/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dto/ParkingSpotDto.java
@@ -9,7 +9,7 @@ public class ParkingSpotDto {
 
     @NotBlank
     @Size(max = 7)
-    private String licencePlateCar;
+    private String licensePlateCar;
 
     @NotBlank
     private String brandCar;
@@ -29,4 +29,68 @@ public class ParkingSpotDto {
     @NotBlank
     private String block;
 
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCart(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCar() {
+        return modelCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public String getColorCar() {
+        return colorCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public String getResponsibleName() {
+        return responsibleName;
+    }
+
+    public void setResponsibleName(String responsibleName) {
+        this.responsibleName = responsibleName;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
 }
