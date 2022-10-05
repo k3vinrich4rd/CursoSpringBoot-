ac862226de25f3d3e560ae1b4d0a75437c443c26
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-04 22:57:05 -0300

    Inserindo implementações feitas no escopo da controller

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -1,2 +1,35 @@
-package com.api.parkingcontrol.controller;public class ParkingSpotController {
+package com.api.parkingcontrol.controller;
+
+import com.api.parkingcontrol.dto.ParkingSpotDto;
+import com.api.parkingcontrol.model.ParkingSpotModel;
+import com.api.parkingcontrol.service.ParkingSpotService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping(path = "/parking-spot")
+public class ParkingSpotController {
+
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+    @PostMapping //Object para possíveis categorias de retorno
+    public ResponseEntity<Object> saveParkingsSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+        var parkingSpotModel = new ParkingSpotModel(); //var substitui a declaração da classe para criar um objeto
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel); //Conversão do Dto para model, onde passamos oque vai ser convertido e no que vai ser convertido
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC"))); //Setando a data de registro pelo sistema
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
+
+
 }
