e6309a3ec6a34372c9804b31c047f8f8378cbef9
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-05 09:57:51 -0300

    Inserindo criação de classe de configuração para requisições Web

--- src/main/java/com/api/parkingcontrol/config/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/config/security/WebSecurityConfig.java
@@ -1,2 +1,19 @@
-package com.api.parkingcontrol.config.security;public class WebSecurityConfig {
+package com.api.parkingcontrol.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration // Para mostrar ao Spring que é uma classe de configuração
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter { //Para fazer configurações customizadas de configuração
+
+    //Onde vamos receber solicitações Http, onde podemos criar restrições para determinadas solicitações
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception{
+        http
+                .httpBasic(). //Http basico
+                and(). // e
+                authorizeRequests(). //Autorização de requisições
+                anyRequest().permitAll(); //Para qualquer requisição, vamos permitir todos os acessos
+    }
 }
