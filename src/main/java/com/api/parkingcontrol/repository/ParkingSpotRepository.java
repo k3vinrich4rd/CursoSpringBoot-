5928ddd40fbb4f8a47b801a349f9f1fe7e2280e4
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-04 21:20:25 -0300

    Inserindo criação da interface, package repository e implementações

--- src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -1,2 +1,20 @@
-package com.api.parkingcontrol.repository;public interface ParkingSpotRepository {
+package com.api.parkingcontrol.repository;
+
+import com.api.parkingcontrol.model.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+    /*
+Estende o Jpa repository para utilizarmos  seus metodos prontos para utilizarmos para transações com o banco de dados
+métodos:
+Listar (Get)
+Salvar(Post)
+Atualizar(Put)
+Deletar(Delete) etc
+
+Importante: Quando estendemos ele já traz a anotação @Repository, então não é necessário colocar a anotação @Repository
+ */
+import java.util.UUID;
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
 }
