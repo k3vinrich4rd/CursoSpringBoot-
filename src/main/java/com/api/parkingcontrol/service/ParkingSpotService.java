84bf9932a4d1926d6108d6cb915dee8c7bfcf951
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-04 22:58:16 -0300

    Inserindo implementações feitas na service

--- src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -1,2 +1,23 @@
-package com.api.parkingcontrol.service;public class ParkingSpotService {
+package com.api.parkingcontrol.service;
+
+import com.api.parkingcontrol.model.ParkingSpotModel;
+import com.api.parkingcontrol.repository.IParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service //Estereótipo
+public class ParkingSpotService {
+
+    //Injeção de dependência
+    final IParkingSpotRepository iParkingSpotRepository;
+
+    public ParkingSpotService(IParkingSpotRepository iParkingSpotRepository) {
+        this.iParkingSpotRepository = iParkingSpotRepository;
+    }
+
+    @Transactional //Garante o rollback em transações caso algo dê errado
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return iParkingSpotRepository.save(parkingSpotModel);
+    }
 }
