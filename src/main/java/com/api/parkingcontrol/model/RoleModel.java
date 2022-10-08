e627fb1c9f7052fc546be280932a5f6055a55423
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-08 18:13:00 -0300

    Inserindo crição e implementações feitas na entidade 'RoleModel'

--- src/main/java/com/api/parkingcontrol/model/RoleModel.java
+++ src/main/java/com/api/parkingcontrol/model/RoleModel.java
@@ -1,2 +1,44 @@
-package com.api.parkingcontrol.model;public class RoleModel {
+package com.api.parkingcontrol.model;
+
+import com.api.parkingcontrol.enums.RoleName;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ROLE")
+public class RoleModel implements GrantedAuthority, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID roleId;
+
+    @Enumerated(EnumType.STRING) //Para converter para o banco como String
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
+
+    @Override
+    public String getAuthority() {
+        return this.roleName.toString(); //Precisa converter para String porque o retorno do método é uma String
+    }
+
+
+    public UUID getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(UUID roleId) {
+        this.roleId = roleId;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
 }
