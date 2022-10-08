fc0b47497e765a9056cfdb526c4b5e1f97418cea
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-08 18:15:33 -0300

    Inserindo implementações e criação de 'IUserRepository'

--- src/main/java/com/api/parkingcontrol/repository/IUserRepository.java
+++ src/main/java/com/api/parkingcontrol/repository/IUserRepository.java
@@ -1,2 +1,14 @@
-package com.api.parkingcontrol.repository;public class IUserRepository {
+package com.api.parkingcontrol.repository;
+
+import com.api.parkingcontrol.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+
+    Optional<UserModel> findByUserName(String userName);
 }
