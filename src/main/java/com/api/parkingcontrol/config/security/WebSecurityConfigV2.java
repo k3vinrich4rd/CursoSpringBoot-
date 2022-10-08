bb153759a5f01e954ed3812277cf0460c8b7a27b
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-08 18:07:19 -0300

    Inserindo classe de configuração, segunda versão

--- src/main/java/com/api/parkingcontrol/config/security/WebSecurityConfigV2.java
+++ src/main/java/com/api/parkingcontrol/config/security/WebSecurityConfigV2.java
@@ -1,2 +1,37 @@
-package com.api.parkingcontrol.config.security;public class WebSecurityConfigV2 {
+package com.api.parkingcontrol.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfigV2 {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                // "**" em casos que temos de passar algum ‘id’ na url do método Http
+                        httpBasic()
+                .and()
+                .authorizeRequests()
+                //Umas das formas de se gerenciar os acessos via roles
+//                .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+//                .antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+                .anyRequest().authenticated() //Todas as solicitações Http precisam ser autenticadas
+                .and()
+                .csrf().disable(); //Desabilitando o csrf
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
